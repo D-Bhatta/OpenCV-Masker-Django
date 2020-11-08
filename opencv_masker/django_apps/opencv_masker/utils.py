bb8f62b0a99cefd7f1452b0d861605604b7288a2
D-Bhatta <dbhatta1232@gmail.com>
2020-11-08 06:52:40 -0500

    Create a file overwriter utility
    We need an utility that can overwrite a list of files.
    
    `write_file_to_files` will overwrite a list of files with one file.

--- opencv_masker/django_apps/opencv_masker/utils.py
+++ opencv_masker/django_apps/opencv_masker/utils.py
@@ -83,3 +83,51 @@ def read_video(name: str, path: str) -> bytes:
             return video_file
     except Exception as e:
         lg.error(str(e))
+
+
+def write_file_to_files(oname: str, opath: str, names: list, paths: list):
+    r"""Overwrite a list of files with one file.
+
+    Overwrite a list of files with names `names` and paths `paths` with a
+    single file with name `oname` and path `opath`.
+
+    Parameters
+    ----------
+    oname : str
+        Name of the file that will overwrite the other files.
+    opath : str
+        Path to the file that will overwrite the other files.
+    names : list
+        List of names of the files that will be overwritten. Each item should
+        be `str`.
+    paths : list
+        List of paths to the files that will be overwritten. Each item should
+        be `str`.
+
+    Returns
+    -------
+    None
+
+    Raises
+    ------
+    FileNotFoundError
+        When file path doesn't exist.
+
+    Examples
+    --------
+    Pass the name and path of the file that wiil overwrite the other files.
+    Pass a list of names and a list of paths of the files that will be
+    overwritten.
+
+    >>> write_file_to_files("a.txt", "/", ["1.txt"], ["/"])
+    """
+
+    o_file = read_video(oname, opath)
+    for name, path in zip(names, paths):
+        file_name = path + name
+        try:
+            with open(file_name, "wb+") as file_writer:
+                file_writer.write(o_file)
+                lg.debug(f"Written file to {file_name}")
+        except Exception as e:
+            lg.error(str(e))
