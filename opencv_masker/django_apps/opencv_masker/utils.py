5a3c8f87684a962e4201aeac6b016c1e2d235794
D-Bhatta <dbhatta1232@gmail.com>
2020-11-08 06:31:39 -0500

    Create file reading utility
    We need to read video files in binary mode. To do this,we have created a
    utility function `read_video`. It reads the file and returns a
    binary object.

--- opencv_masker/django_apps/opencv_masker/utils.py
+++ opencv_masker/django_apps/opencv_masker/utils.py
@@ -44,3 +44,42 @@ def store_file(name: str, path: str, file: TemporaryUploadedFile):
         lg.debug(f"Written file to {file_name}")
     except Exception as e:
         lg.error(str(e))
+
+
+def read_video(name: str, path: str) -> bytes:
+    r"""Reads the video file and returns a binary object.
+
+    This function reads the video file with name `name` at path `path` as a
+    binary object and returns it.
+
+    Parameters
+    ----------
+    name : str
+        A string that specifies the file name.
+    path : str
+        A string that represents the path to the object.
+
+    Returns
+    -------
+    video_file : bytes
+        THe binary representation of the video file.
+
+    Raises
+    ------
+    FileNotFoundError
+        When file path doesn't exist.
+
+    Examples
+    --------
+    Pass a file name and a path to the object.
+
+    >>> print(read_video("a.txt", "/").decode())
+    Hello world
+    """
+    file_name = path + name
+    try:
+        with open(file_name, "rb+") as file_reader:
+            video_file = file_reader.read()
+            return video_file
+    except Exception as e:
+        lg.error(str(e))
